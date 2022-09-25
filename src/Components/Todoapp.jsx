74439e1942d6a8b69838f1687c4c47869cbc0ee5
puranchandralohar <clpuran95@gmail.com>
2022-09-25 18:22:46 +0530

    Functionalities Added

--- src/Components/Todoapp.jsx
+++ src/Components/Todoapp.jsx
@@ -1,18 +1,51 @@
 import React from 'react'
-import "./Style.css"
+import "./Style.css";
+import { useState } from 'react';
 
 
 export function Todoapp() {
+    const [todos, setTodos] = useState([]);
+    const [list, setLists] = useState("");
+
+    const addlist = () => {
+    if(list.length <= 0) alert("Empty Field");
+    else{
+      setTodos([...todos, list]);
+      setLists("");
+    }
+    };
+
   return (
     <div className='Wrapper'>
+    <div className="heading"><h1>Todo App</h1></div>
         <div className='todoApp'>
             <input
             type="text"
             name='todo'
+            value={list}
             placeholder={"Enter Your Todo"}
+            onChange={(e) => setLists(e.target.value)}
             />
-            <button>Add Todo</button>
-        </div>       
+            <button onClick={addlist}>Add Todo</button>
+        </div>
+        <div className='todo_lists'>
+            {todos.map((todo) => (
+                <div>
+                    <span>{todo}</span>
+                    <button className='btn edit' onClick={()=>{
+                        
+                    }}>Edit</button>
+                    <button className='btn del'
+                        onClick={() => {
+                            let filteredItems = todos.filter((value) => value !== todo);
+                            setTodos([...filteredItems]);
+                        }}
+                        >
+                        Delete
+                    </button>
+                </div>
+            ))}
+        </div>          
     </div>
   )
 }
